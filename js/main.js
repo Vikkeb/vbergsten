9f8a4cc719d2654bc9445a206b717c77399d44f4
Victor Bergsten <victor.bergsten@ericsson.com>
2018-04-23 14:17:20 +0200

    First Commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function($) {
+
+  $('#fullpage').fullpage({
+    menu: '#pure-menu-list',
+    anchors:['about', 'work', 'skills','contact'],
+    scrollBar: true
+  });
+
+
+  $(window).on("load", function(){
+    $('#preloader').fadeOut(1200,function(){$(this).remove();});
+    });
+    $('.my-gallery').slick({
+      lazyLoad: 'ondemand',
+      infinite: 'false',
+      slidesToShow: 4,
+      slidesToScroll: 4
+    });
+    $('.my-gallery').slickLightbox({
+      itemSelector: 'li > a'
+    });
+
+});
+
+  window.onload = function onLoad() {
+    var circle = new ProgressBar.Circle('#photoshop', { color: '#07caff', duration: 3000, easing: 'easeInOut'});
+    circle.animate(0.9);
+    var circle = new ProgressBar.Circle('#illustrator', { color: '#fa7600', duration: 3000, easing: 'easeInOut'});
+    circle.animate(0.7);
+    var circle = new ProgressBar.Circle('#indesign', { color: '#ff3891', duration: 3000, easing: 'easeInOut'});
+    circle.animate(0.95);
+    var circle = new ProgressBar.Circle('#axure', { color: '#a60000', duration: 3000, easing: 'easeInOut'});
+    circle.animate(0.8);
+    var circle = new ProgressBar.Circle('#balsamiq', { color: '#a502b3', duration: 3000, easing: 'easeInOut'});
+    circle.animate(1);
+    var circle = new ProgressBar.Circle('#programming', { color: 'rgba(0, 0, 0, 0.87)', duration: 3000, easing: 'easeInOut'});
+    circle.animate(0.75);
+  };
